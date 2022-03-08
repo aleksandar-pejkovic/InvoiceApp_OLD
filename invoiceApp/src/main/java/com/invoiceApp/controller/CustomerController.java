284b1ddb2744a16ed5eb1200cc0bc6448fedffe2
LotusSRB <36522854+LotusSRB@users.noreply.github.com>
2022-03-08 21:03:35 +0100

    Added links using spring hateoas

--- invoiceApp/src/main/java/com/invoiceApp/controller/CustomerController.java
+++ invoiceApp/src/main/java/com/invoiceApp/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.invoiceApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.invoiceApp.entity.Customer;
+import com.invoiceApp.entity.Invoice;
 import com.invoiceApp.service.CustomerService;
+import com.invoiceApp.service.InvoiceService;
 
 @RestController
 @RequestMapping("/customers")
@@ -22,11 +26,14 @@ public class CustomerController {
 	@Autowired
 	CustomerService service;
 
+	@Autowired
+	InvoiceService invoiceService;
+
 	@PostMapping
 	public Customer createCustomer(@RequestBody Customer customer) {
 		return service.newCustomer(customer);
 	}
-	
+
 	@PostMapping("/all")
 	public List<Customer> createCustomers(@RequestBody List<Customer> customers) {
 		return service.newCustomers(customers);
@@ -55,6 +62,38 @@ public class CustomerController {
 	@GetMapping("/pib/{pib}")
 	public Customer findCustomerByPib(@PathVariable String pib) {
 		return service.findByPib(pib);
+	}
+
+	@GetMapping("/name/{name}/invoices")
+	public List<Invoice> getCustomerInvoices(@PathVariable String name) {
+
+		return service.findByName(name).getInvoices();
+	}
+
+	@GetMapping("/name/{name}/invoices/{invoiceId}")
+	public Invoice getUserInvoice(@PathVariable String name, @PathVariable Long invoiceId) {
+
+		Invoice returnValue = invoiceService.findById(invoiceId);
+		
+		Link customerLink = WebMvcLinkBuilder.linkTo(Customer.class)
+				.slash(name)
+				.withRel("customer");
+		Link customerInvoicesLink = WebMvcLinkBuilder.linkTo(Customer.class)
+				.slash(name)
+				.slash("invoices")
+				.withRel("invoices");
+		Link selfLink = WebMvcLinkBuilder.linkTo(Customer.class)
+				.slash(name)
+				.slash("invoices")
+				.slash(invoiceId)
+				.withSelfRel();
+		
+		returnValue.add(customerLink);
+		returnValue.add(customerInvoicesLink);
+		returnValue.add(selfLink);
+		
+		return returnValue;
+		
 	}
 
 	@GetMapping
